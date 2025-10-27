4d5cb6e9c8d44ff52ba7e5b6a01aaeaa9995e464
Anthony oliveira <antholi0203@gmail.com>
2025-10-27 16:05:52 -0300

    Add files via upload
    aula 27/10

---
+++ Java_StarTech/comandos_java/src/Exercicios/exCombo.java
@@ -0,0 +1,13 @@
+package Exercicios;
+
+import javax.swing.text.html.parser.Parser;
+import  java.util.Scanner;
+
+
+
+public class exCombo {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Java_StarTech/comandos_java/src/Input.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Input {
+    public static void main(){
+
+
+
+        System.out.println("Seu peso");
+    }
+}
